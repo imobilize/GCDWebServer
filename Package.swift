2cf4dcb24c3c3b55861ee653ff3b564830d6f698
Andre Barrett <andrebarrett@gmail.com>
2020-11-30 11:26:57 +0000

    Create Package.swift
    Added support for swift package manager

---
+++ Package.swift
@@ -0,0 +1,22 @@
+
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "GCDWebServer",
+    platforms: [
+          .iOS(.v9),
+          .tvOS(.v9),
+          .macOS(.v10_11),
+    ],
+    products: [
+        .library(
+            name: "GCDWebServer",
+            targets: ["GCDWebServer"])
+    ],
+    targets: [
+        .target(name: "GCDWebServer")
+    ],
+    swiftLanguageVersions: [.v5]
+)
